12980cbb0c81e6c06472611af14472a238921333
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-08-30 02:07:19 +0300

    Add import for lucide-react and define testConflit variable in ReactQueryProvider

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,12 +1,14 @@
 import { queryClient } from "@/lib/react-query/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { X } from "lucide-react";
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const testConflit = 20;
   return (
     <QueryClientProvider client={queryClient}>
       {children}
